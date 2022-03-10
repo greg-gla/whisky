eeb6cdb7802624f9588c45f05a866f22437c8d8b
ChuTianshu717 <cts717cs@gmail.com>
2022-03-10 23:00:48 +0000

    The login and registration views are complete

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ app_name = 'whisky'
 
 urlpatterns = [
     path('', views.IndexView, name='index'),
-    path('register/', views.register, name='register'),
+    path('register/', views.regist, name='register'),
     path('login/', views.user_login, name='login'),
     # path('logout/', views.user_logout, name='logout'),
 
--- pages/views.py
+++ pages/views.py
@@ -1,63 +1,72 @@
-from django.shortcuts import render
+import tkinter.messagebox
+import time
+from random import random
+from tkinter import *
+
+from django.contrib.auth import login
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
-
 # Create your views here.
+
+from pages import models
+from pages.forms import RegisterForm
+
+
 class IndexView(View):
     def get(self, request, **kwargs):
         return render(request, 'pages/index.html')
 
 
-def register(request):
-    # registered = False
-    # if request.method == 'POST':
-    #     user_form = UserForm(request.POST)
-    #     profile_form = UserProfileForm(request.POST)
-	#
-    #     if user_form.is_valid() and profile_form.is_valid():
-    #         user = user_form.save(commit=False)
-	#
-    #         user.set_password(user.password)
-    #         user.save()
-	#
-    #         profile = profile_form.save(commit=False)
-    #         profile.user = user
-	#
-    #         if 'picture' in request.FILES:
-    #             profile.picture = request.FILES['picture']
-	#
-    #         profile.save()
-    #         registered = True
-    #     else:
-    #         print(user_form.errors, profile_form.errors)
-    # else:
-    #     user_form = UserForm()
-    #     profile_form = UserProfileForm()
-
-    return render(request,'pages/registration.html')
-                  # context={'user_form': user_form,
-                  #          'profile_form': profile_form,
-                  #          'registered': registered})
+def regist(request):
+    if request.method == 'GET':
+        return render(request, 'pages/registration.html')
+    if request.method == 'POST':
+        # 注册
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password')
+        password2 = request.POST.get('re_password')
+        if password1 != password2:
+            tkinter.messagebox.showinfo('Hint', 'The two passwords are inconsistent!')
+            return render(request, 'pages/registration.html')
+        else:
+            same_name_user = models.User.objects.filter(nickname=name)
+            if same_name_user:
+                tkinter.messagebox.showinfo('Hint', 'The user name is occupied!')
+                return render(request, 'pages/registration.html')
+            same_email_user = models.User.objects.filter(email=email)
+            if same_email_user:
+                tkinter.messagebox.showinfo('Hint', 'The email address has been registered!')
+                return render(request, 'pages/registration.html')
+            password1 = make_password(password1)
+            new_user = models.User()
+            new_user.nickname = name
+            new_user.email = email
+            new_user.password = password1
+            new_user.save()
+            return HttpResponseRedirect('/whisky/login/')
 
 
 def user_login(request):
-    # if request.method == 'POST':
-    #     username = request.POST.get('username')
-    #     password = request.POST.get('password')
-    #     user = authenticate(username=username, password=password)
-	#
-    #     if user:
-    #         if user.is_active:
-    #             login(request, user)
-    #             return redirect(reverse('rango:index'))
-    #         else:
-    #             return HttpResponse("Your Rango account is disabled.")
-    #     else:
-    #         print(f"Invalid login details: {username}, {password}")
-    #         return HttpResponse("Invalid login details supplied.")
-    # else:
+    if request.method == 'GET':
         return render(request, 'pages/login.html')
 
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if models.User.objects.filter(email=email).exists():
+            user = models.User.objects.get(email=email)
+            if check_password(password, user.password):
+                return render(request, 'pages/index.html')
+            else:
+                tkinter.messagebox.showinfo('Hint', 'Incorrect password')
+                return render(request, 'pages/login.html')
+        else:
+            tkinter.messagebox.showinfo('Hint', 'The email address does not exist')
+            return render(request, 'pages/login.html')
 
 # @login_required
 # def user_logout(request):
